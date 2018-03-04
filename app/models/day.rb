d2f1e1f4c4c81eb991e9585181cadb182c830e1e
aback1227 <aback1227@gmail.com>
2018-03-04 17:56:47 -0500

    day uniqueness true

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,5 +2,6 @@ class Day < ApplicationRecord
   belongs_to :itinerary
   has_many :activities
   validates :date, presence: true
+  validates :date, uniqueness: true
   validates :title, presence: true
 end
