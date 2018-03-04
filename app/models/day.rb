351c27a959b04784f34759c69c00887bb5b58049
aback1227 <aback1227@gmail.com>
2018-03-04 18:13:51 -0500

    validate date is within trip range

--- app/models/day.rb
+++ app/models/day.rb
@@ -4,4 +4,12 @@ class Day < ApplicationRecord
   validates :date, presence: true
   validates :date, uniqueness: true
   validates :title, presence: true
+  validate :within_date_range
+
+  def within_date_range
+    itinerary = Itinerary.find_by_id(self.itinerary_id)
+    if date < itinerary.datestart || date > itinerary.dateend
+      errors.add(:date, "not within trip date range")
+    end
+  end
 end
