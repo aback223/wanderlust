7e1a1f130c11f75445f191a822c3c5cdd33ffa7b
aback1227 <aback1227@gmail.com>
2018-03-04 23:25:46 -0500

    find or create categories in form

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,12 @@ class Activity < ApplicationRecord
   validates :title, presence: true
   has_many :activity_categories
   has_many :categories, through: :activity_categories
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 end
