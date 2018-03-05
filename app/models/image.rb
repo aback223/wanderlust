434e41105a0a0f6ee414bae6cbb59e2644706402
aback1227 <aback1227@gmail.com>
2018-03-04 19:10:18 -0500

    validation errors for image form

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,8 @@
 class Image < ApplicationRecord
   belongs_to :itinerary
   validates :url, allow_blank: true, format: {
-    with: %r{\.gif|jpg|png|jpeg}, 
+    with: /\.gif|jpg|png|jpeg/, 
     message: "must be a url for gif, jpg, or png image."
   }
+  validates :url, presence: true
 end
