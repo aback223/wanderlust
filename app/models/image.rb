e620bde7afcaa734066c8dee432709b1745bd59b
aback1227 <aback1227@gmail.com>
2018-03-04 20:04:26 -0500

    changes to custom error msg

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ApplicationRecord
   belongs_to :itinerary
   validates :url, format: {
     with: /\.gif|jpg|png|jpeg/, 
-    message: "must be a url for gif, jpg, or png image."
+    message: "must link to a gif, jpg, or png image."
   }
   validates :url, presence: true
 end
