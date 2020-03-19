1328308a364225da67141c98199b10fad0e42222
aback223 <aback1227@gmail.com>
2020-03-19 02:57:59 -0400

    sign up form with validation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,28 @@
 class WelcomeController < ApplicationController
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
   def home
+    @minimum_password_length = User.password_length.min
     render :layout => false
   end
 
   def privacy
+  end
+
+  private 
+
+  def resource_name 
+    :user
+  end
+
+  def resource
+    @resource ||=User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+ 
+  def resource_class
+    User
   end
 end
